8bfdd51cd22bbb593138b25fed14dc298e780487
alexandrute <teodoroviciwork@gmail.com>
2018-06-21 13:14:23 +0300

    update comment

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+//we call an action creator when we want to invoke a change. kinda like setState. 
+//the action creator returns an action that is sent to all reducers wich in turn calculate the new app state. 
+//the new app state is sent to all components that are re-rendered with the new changes.
 //an action creator is a javascript function that returns actions.
 //we pass in the libraryId of the library we want to select.
 export const selectLibrary = (libraryId) => {
