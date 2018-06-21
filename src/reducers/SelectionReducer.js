9b2c110919129120485a83c855347416b0814e34
alexandrute <teodoroviciwork@gmail.com>
2018-06-21 14:00:47 +0300

    selection reducer update

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,7 +1,8 @@
 //reducers are called with the state object and an action.
 //the first time this reducer runs, it will have the state as undefined.
 //so we initialize the state with a null value.
-export default (state = null, action) => {
+const INITIAL_STATE = null;
+export default (state = INITIAL_STATE, action) => {
     console.log(action);
     //this function contains a switch over the action.type where we decide what action to return.
     switch (action.type) {
