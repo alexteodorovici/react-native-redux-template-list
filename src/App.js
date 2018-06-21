23e21ff676f353102d814398a2df7a6c21715757
alexandrute <teodoroviciwork@gmail.com>
2018-06-21 10:36:51 +0300

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-
+import { createStore } from 'redux';
 import LibraryList from './components/LibraryList';
-
 import reducers from './reducers';
-
-//import LibraryReducer from './reducers/LibraryReducer';
-// const reducers = combineReducers({
-//     //we asign the LibraryReducer to the key called libraries.
-//     libraries: LibraryReducer
-// });
-
 
 const App = () => {
     return (
@@ -23,7 +14,7 @@ const App = () => {
             {/* this is the GUI interface of the app */}
             <View >
                 <Text> React Native Redux Template </Text>
-                <LibraryList/>
+                <LibraryList />
             </View>
         </Provider>
     );
